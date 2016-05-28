c92617b308b656b5d04ba67dbccdfbea37301a1a
Tobias Grosser <tobias@grosser.es>
2016-05-28 10:33:39 +0200

    Create a context object

--- isl.py
+++ isl.py
@@ -1,28 +1,30 @@
 import js
-ctx = js.globals.Module._isl_ctx_alloc()
-class Set:
-  def __init__(self, ptr):
-    self.ptr = ptr
-  @staticmethod
-  def from_str(string):
-    retType = 'number'
-    argTypes = ['number', 'string']
-    args = [ctx, string]
-    ptr = js.globals.Module.ccall('isl_set_read_from_str', retType, argTypes, args)
-    return Set(ptr)
-  def __repr__(self):
-    p = js.globals.Module._isl_printer_to_str(ctx)
-    p = js.globals.Module._isl_printer_print_set(p, self.ptr)
-    string = js.globals.Module.ccall('isl_printer_get_str', 'string', ['number'], [p])
-    return str(string)
-  def intersect(self, set2):
-     ptr1 = js.globals.Module._isl_set_copy(self.ptr)
-     ptr2 = js.globals.Module._isl_set_copy(set2.ptr)
-     ptr = js.globals.Module._isl_set_intersect(ptr1, ptr2)
-     return Set(ptr)
-  def union(self, set2):
-     ptr1 = js.globals.Module._isl_set_copy(self.ptr)
-     ptr2 = js.globals.Module._isl_set_copy(set2.ptr)
-     ptr = js.globals.Module._isl_set_union(ptr1, ptr2)
-     return Set(ptr)
 
+class Error(Exception):
+    pass
+
+class Context:
+    defaultInstance = None
+
+    def __init__(self):
+        retType = 'number'
+        argTypes = []
+        args = []
+        ptr = js.globals.Module.ccall('isl_ctx_alloc', retType, argTypes, args)
+        self.ptr = ptr
+
+    def __del__(self):
+        retType = None
+        argTypes = [number]
+        args = [self.ptr]
+        ptr = js.globals.Module.ccall('isl_ctx_free', retType, argTypes, args)
+        isl.isl_ctx_free(self)
+
+    def from_param(self):
+        return self.ptr
+
+    @staticmethod
+    def getDefaultInstance():
+        if Context.defaultInstance == None:
+            Context.defaultInstance = Context()
+        return Context.defaultInstance
